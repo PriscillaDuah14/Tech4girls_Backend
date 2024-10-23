462352d9f65e40033ce811101984f63e10925a3e
PriscillaDuah14 <priscillaantwiwaaduah@gmail.com>
2024-10-23 14:05:33 +0000

    package installations and update

---
+++ Bash_Scripting/install_packages.sh
@@ -0,0 +1,12 @@
+#!bin/bash
+
+#this code installs fortune,cowsay and cmatrix
+sudo apt install cowsay
+sudo apt install fortune
+sudo apt install cmatrix
+
+#update command
+sudo apt update cmatrix
+sudo apt install fortune
+sudo apt install cmatrix
+
