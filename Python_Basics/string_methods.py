d92c56f56a76f3325e105042278f42dce7630ffb
PriscillaDuah14 <bhaeprissy@gmail.com>
2024-10-28 13:18:55 -0700

    python is fun

---
+++ Python_Basics/string_methods.py
@@ -0,0 +1,8 @@
+#python strings
+
+my_string = "hello, my friend!"
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.join('python is fun'))
+print(my_string.replace('friend','python'))
+
