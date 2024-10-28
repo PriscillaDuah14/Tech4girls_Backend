42f65f65722a520b2f0efca495a6678655c3c74e
PriscillaDuah14 <bhaeprissy@gmail.com>
2024-10-28 13:29:20 -0700

    python is product

---
+++ Python_Basics/f_string_expressions.py
@@ -0,0 +1,11 @@
+#python expressions
+
+a = 10
+b = 30
+sum_result = a + b
+difference_result = a - b
+product_result = a * b 
+
+print(f" the sum of {a} and {b},is {sum_result}")
+print(f"the difference of {a} and {b}is {difference_result}")
+print(f"the product of {a} and {b} is {product_result}")
