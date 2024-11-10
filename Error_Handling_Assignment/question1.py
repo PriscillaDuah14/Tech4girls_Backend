07ee272633a9116cb5f577f4e6bfdaab5d9f6f85
PriscillaDuah14 <bhaeprissy@gmail.com>
2024-11-09 20:12:57 -0800

    zero division error

--- Error_Handling_Assignment/question1.py
+++ Error_Handling_Assignment/question1.py
@@ -1,17 +1,17 @@
+
 def division_calculator():
     try:
         numerator = int(input("Enter the numerator: "))
-        denominator = int(input("Enter the denominator: "))
-        
-        if denominator == 0:
-            print("Error: Division by zero is not allowed.")
-            return
-        
+        denominator = int(input("Enter the denominator: ")) 
+         
         result = numerator / denominator
+    except ZeroDivisionError:
+        print("Error: Division by zero is not allowed.")
+        return
+    except ValueError:
+        print("Error: Please enter valid numbers.")
+        return
+    else:
         print("The result is:", result)
         
-    except ValueError:
-        print("Error: Please enter valid integers.")
-
-
 division_calculator()
