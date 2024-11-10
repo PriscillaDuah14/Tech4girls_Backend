c2f45e10af11be6c0a27eab607915d3d65d38ab5
PriscillaDuah14 <bhaeprissy@gmail.com>
2024-11-09 19:26:53 -0800

    value error

---
+++ Error_Handling_Assignment/question3.py
@@ -0,0 +1,9 @@
+#python
+user_input = input("Enter a number: ")
+
+try:
+    converted_number = int(user_input)
+    print("The number you entered is:", converted_number)
+except ValueError:
+    print("Error: Please enter a valid integer.")
+
