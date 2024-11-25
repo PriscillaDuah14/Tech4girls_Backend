dc0d80497acfabcff616601ba9cbbfb49f6c52a3
PriscillaDuah14 <bhaeprissy@gmail.com>
2024-11-24 17:04:04 -0800

    Building Relationships

--- SQL_Assignment/question2.sql
+++ SQL_Assignment/question2.sql
@@ -28,6 +28,14 @@ VALUES
 (3, 'User 3 Post', 'User 3 has posted this content.');
 
 SELECT * FROM Posts;
-
+--OUTPUT
++----+---------+------------------+----------------------------------------+---------------------+
+| id | user_id | title            | content                                | created_at          |
++----+---------+------------------+----------------------------------------+---------------------+
+|  1 |       1 | My First Post    | This is the content of the first post. | 2024-11-24 16:50:19 |
+|  2 |       1 | Another Post     | Here is another post from user 1.      | 2024-11-24 16:50:19 |
+|  3 |       2 | A Post by User 2 | Content of user  post.                 | 2024-11-24 16:50:19 |
+|  4 |       3 | User 3 Post      | User 3 has posted this content.        | 2024-11-24 16:50:19 |
++----+---------+------------------+----------------------------------------+---------------------+
 
 
